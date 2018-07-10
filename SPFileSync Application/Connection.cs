931141be5dc7836b23daa4e1ded55c2ec821969b
Radu Todor <todorradu@gmail.com>
2018-07-10 17:30:59 +0300

    Needed references added and Connection class implemented with whole sharepoint site.

---
+++ SPFileSync Application/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Configuration;
+using Microsoft.SharePoint.Client;
+
+namespace SPFileSync_Application
+{
+    public class Connection
+    {
+        public Connection(Uri uri, NetworkCredential credentials)
+        {
+            Uri = uri;
+            Credentials = credentials;
+        }
+
+        public static Uri Uri = new Uri(ConfigurationManager.AppSettings["SharePointURL"]);
+
+        public static NetworkCredential Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Account"], ConfigurationManager.AppSettings["Password"]);
+
+        public static Web SharePointResult()
+        {
+            ClientContext context = new ClientContext(Uri);
+            context.Credentials = Credentials;
+            Web site = context.Web;
+            return site;
+        }
+    }
+}
