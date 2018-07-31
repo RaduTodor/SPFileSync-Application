8d17af0442ae45fb16ff85bebee7b1e923ee2ab8
Ilies Ciprian <iliesciprian94@gmail.com>
2018-07-31 11:32:50 +0300

    CR
    CR

--- BusinessLogicLayer/FileSynchronizer.cs
+++ BusinessLogicLayer/FileSynchronizer.cs
@@ -17,6 +17,7 @@
     /// </summary>
     public class FileSynchronizer
     {
+        //TODO [CR BT]: remove unused code
         private const string Backslash = "\\";
 
         public FileSynchronizer(ConnectionConfiguration configuration, ListReferenceProviderType type, int count)
